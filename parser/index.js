9e82beb61714310aeab4feb2f1fe5faa0d6461fc
Oleh Kravchuk <kolegm.real@gmail.com>
2014-08-12 07:54:56 +0300

    Changed parser

--- parser/index.js
+++ parser/index.js
@@ -24,12 +24,15 @@
  *    "UNKNOWN_ERROR" indicates that the request could not be processed due to a server error. The request may succeed if you try again.
  */
 
-//var util = require('util');
-var _ = require('underscore');
+const OK = 'OK';
+const ZERO_RESULTS = 'ZERO_RESULTS';
+const OVER_QUERY_LIMIT = 'OVER_QUERY_LIMIT';
+const REQUEST_DENIED = 'REQUEST_DENIED';
+const INVALID_REQUEST = 'INVALID_REQUEST';
+const UNKNOWN_ERROR = 'UNKNOWN_ERROR';
 
-module.exports.parseData = function (data) {
-  return data;
-};
+var util = require('util');
+var _ = require('underscore');
 
 module.exports.parseError = function (error) {
   if (error && _.isObject(error)) {
@@ -41,3 +44,87 @@ module.exports.parseError = function (error) {
   }
   return error;
 };
+
+module.exports.parseData = function (data) {
+  var result = {};
+  if (_.isObject(data)) {
+    switch (data.status) {
+      case OK:
+        result = parseResults(data.results);
+        break;
+      case ZERO_RESULTS:
+      case OVER_QUERY_LIMIT:
+      case REQUEST_DENIED:
+      case INVALID_REQUEST:
+      case UNKNOWN_ERROR:
+      default:
+    }
+  }
+  return result;
+};
+
+function parseResults (result) {
+  var data = [];
+  if (_.isArray(result)) {
+    _.each(result, function (info) {
+      //console.log(info.formatted_address);
+      //console.log(info.types);
+      //console.log(util.inspect(info));
+      data.push(format(info));
+    });
+  }
+  return data;
+}
+
+function format (result) {
+  var country = null;
+  var countryCode = null;
+  var city = null;
+  var state = null;
+  var stateCode = null;
+  var zipcode = null;
+  var streetName = null;
+  var streetNumber = null;
+  for (var i = 0; i < result.address_components.length; i++) {
+  // Country
+  if (result.address_components[i].types.indexOf('country') >= 0) {
+  country = result.address_components[i].long_name;
+  }
+  if (result.address_components[i].types.indexOf('country') >= 0) {
+  countryCode = result.address_components[i].short_name;
+  }
+  // State
+  if (result.address_components[i].types.indexOf('administrative_area_level_1') >= 0) {
+  state = result.address_components[i].long_name;
+  }
+  if (result.address_components[i].types.indexOf('administrative_area_level_1') >= 0) {
+  stateCode = result.address_components[i].short_name;
+  }
+  // City
+  if (result.address_components[i].types.indexOf('locality') >= 0) {
+  city = result.address_components[i].long_name;
+  }
+  // Adress
+  if (result.address_components[i].types.indexOf('postal_code') >= 0) {
+  zipcode = result.address_components[i].long_name;
+  }
+  if (result.address_components[i].types.indexOf('route') >= 0) {
+  streetName = result.address_components[i].long_name;
+  }
+  if (result.address_components[i].types.indexOf('street_number') >= 0) {
+  streetNumber = result.address_components[i].long_name;
+  }
+  }
+  return {
+  'latitude' : result.geometry.location.lat,
+  'longitude' : result.geometry.location.lng,
+  'country' : country,
+  'city' : city,
+  'state' : state,
+  'stateCode' : stateCode,
+  'zipcode' : zipcode,
+  'streetName': streetName,
+  'streetNumber' : streetNumber,
+  'countryCode' : countryCode
+  };
+}
